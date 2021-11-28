3dc63aa6a5f8b4e0c24392381793605e25d26b7a
lekanda <abernaolao@gmail.com>
2021-11-28 15:06:46 +0100

    Fetch API.

---
+++ 02-intro-javascript/src/bases/10-fetch.js
@@ -0,0 +1,19 @@
+
+const apiKey = 'Fy5o4SLF3Q2ij74a1bG1N3NoL20R8Cv8';
+
+const peticion = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+
+peticion
+    .then( resp => resp.json() )
+    .then( ({ data }) => {
+        const { url } = data.images.original;
+        
+        const img = document.createElement('img');
+        img.src = url;
+
+        document.body.append( img );
+
+
+    })
+    .catch( console.warn );
+
