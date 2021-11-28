cc8320d3639b3ca02e73723df1e6d32b8346859d
lekanda <abernaolao@gmail.com>
2021-11-28 15:41:07 +0100

    Ternarios en JS.

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,29 +1,16 @@
+const activo = true;
+
+// let mensaje = '';
+
+// if ( activo ) {
+//     mensaje = 'Activo';
+// } else {
+//     mensaje = 'Inactivo';
+// }
+
+// const mensaje = (activo) ? 'Activo' : 'Inactivo';
+const mensaje = ( activo ) ? 'Activo' : null; 
+// const mensaje = activo && 'Activo';
 
 
-const persona = {
-    nombre: 'Tony',
-    apellido: 'Stark',
-    edad: 45,
-    direccion: {
-        ciudad: 'New York',
-        zip: 55321321,
-        lat: 14.3232,
-        lng: 34.9233321
-    }
-};
-
-// console.table( persona );
-
-const persona2 = { ...persona };
-persona2.nombre = 'Peter';
-
-
-console.log( persona );
-console.log( persona2 )
-
-
-
-
-
-
-
+console.log(mensaje);
