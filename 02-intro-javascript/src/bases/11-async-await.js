30b2076709be73c312f67a0b9971ab484591f9c7
lekanda <abernaolao@gmail.com>
2021-11-28 15:31:05 +0100

    Async & Await.

---
+++ 02-intro-javascript/src/bases/11-async-await.js
@@ -0,0 +1,32 @@
+
+
+// const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
+// getImagenPromesa().then( console.log );
+
+const getImagen = async() => {
+
+    try {
+
+        const apiKey = 'Fy5o4SLF3Q2ij74a1bG1N3NoL20R8Cv8';
+        const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+        const { data } = await resp.json(); 
+
+        const { url } = data.images.original;
+
+        const img = document.createElement('img');
+        img.src = url;
+        document.body.append( img );
+
+    } catch (error) {
+        // manejo del error
+        console.error(error)
+    }
+    
+    
+    
+}
+
+ getImagen();
+
+
+
