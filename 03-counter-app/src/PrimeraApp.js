29da3872a9d2ced03f16f6c80bf864f95c1154e1
lekanda <abernaolao@gmail.com>
2021-11-29 10:11:09 +0100

    Imprimir variables en HTML.

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,11 +1,20 @@
 import { Fragment } from "react";
 
 const PrimeraApp = () => {
+
+    const saludo = {
+        nombre: "Juan",
+        apellido: "Perez"
+    };
+
+
+
     return (
-        <Fragment >
-            <h1>Hola Mundo</h1>
+        <>
+            <h1>Hola {saludo.nombre} {saludo.apellido}</h1>
+            {/* <pre>{JSON.stringify(saludo,null,3)}</pre> */}
             <p>Mi primera App</p>
-        </Fragment>
+        </>
     );
 }
 
