8ddee73b856e1e59d56135c05cf6801e98954fd6
lekanda <abernaolao@gmail.com>
2021-11-30 10:27:13 +0100

    Hook-useState.

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,21 +1,27 @@
+import React,{useState} from 'react';
+
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value}) => {
 
+    const [counter,setCounter] = useState(0);
+
+
+
     //HandleAdd
-    const handleAdd = (e) => {
-        // console.log(e);
-        return ()=>{console.log('Hola Mundo');};
+    const handleAdd = () => {
+        // setCounter(counter + 1);
+        setCounter((c) => c + 1);
     }
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
 
             {/* Hacen lo mismo el 'e' se pasa automaticamente */}
-            <button onClick={(e) => {handleAdd(e)}}>+1</button> 
-            <button onClick={handleAdd()}>+2</button> 
+            {/* <button onClick={(e) => {handleAdd(e)}}>+1</button>  */}
+            <button onClick={handleAdd}>+1</button> 
         </>
     );
    
