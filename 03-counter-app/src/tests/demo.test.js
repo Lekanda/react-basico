bb3db14ea89c200a3fc0c476dfd06bb0a7d5c088
lekanda <abernaolao@gmail.com>
2021-11-30 16:09:26 +0100

    JEST. Expect. toBe.

--- 03-counter-app/src/tests/demo.test.js
+++ 03-counter-app/src/tests/demo.test.js
@@ -1,9 +1,15 @@
-test('debe ser  true', () => {
-    const isActive = true;
-
-    if (isActive) {
-        throw new Error('No es activo');
-    }
-        
+describe('Pruebas en el archivo demo.test.js',()=>{
+    test('debe ser  iguales los string', () => {
+        // 1.Inicializacion
+        const mensaje = 'Hola mundo';
     
+        // 2.Estimulo
+        const mensaje2 = `Hola mundo`;
+    
+        // 3.Observar el comportamiento
+        expect(mensaje).toBe(mensaje2);// Tipo y contenido
+    })
 })
+
+
+
