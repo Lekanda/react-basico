8a1f34c1314bf5f618ca3046a4ad13e2989f5b6d
lekanda <abernaolao@gmail.com>
2021-11-30 09:57:56 +0100

    Evento Click

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -2,11 +2,20 @@ import PropTypes from 'prop-types';
 
 const CounterApp = ({ value}) => {
 
+    //HandleAdd
+    const handleAdd = (e) => {
+        // console.log(e);
+        return ()=>{console.log('Hola Mundo');};
+    }
 
     return (
         <>
             <h1>CounterApp</h1>
             <h2> { value } </h2>
+
+            {/* Hacen lo mismo el 'e' se pasa automaticamente */}
+            <button onClick={(e) => {handleAdd(e)}}>+1</button> 
+            <button onClick={handleAdd()}>+2</button> 
         </>
     );
    
