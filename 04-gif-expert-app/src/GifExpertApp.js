0a297a3c8623ac54a419513d843f194c8856d815
lekanda <abernaolao@gmail.com>
2021-12-03 14:20:02 +0100

    GifExpertApp Componente.

---
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+
+
+export const GifExpertApp = () => {
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr/>
+        </>
+    )
+}
+
+export default GifExpertApp
