a9d6bfb452da63fb63faf207dabc43cca9006b2f
lekanda <abernaolao@gmail.com>
2021-12-03 15:59:11 +0100

    Componente AdCategory.

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,24 +1,23 @@
 import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 // import ReactDOM from 'react-dom';
 
 export const GifExpertApp = () => {
 
-    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
-    
     const [categories, setcategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
 
-    const handleAdd = () => {
-        // categories.push('HunterXHunter');
-        // console.log(categories);
-        // setcategories([...categories, 'HunterXHunter']);
-        setcategories( cats => [...cats, 'HunterXHunter']);
-    }
+    // const handleAdd = () => {
+    //     // categories.push('HunterXHunter');
+    //     // console.log(categories);
+    //     // setcategories([...categories, 'HunterXHunter']);
+    //     setcategories( cats => [...cats, 'HunterXHunter']);
+    // }
     
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr/>
-            <button onClick={ handleAdd }>Agregar</button>
+            <AddCategory />
             <hr/>
             <ol>
                 {  
