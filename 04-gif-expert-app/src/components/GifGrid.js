02f29a5072e9882daa6e985102d682f2dea945de
lekanda <abernaolao@gmail.com>
2021-12-05 13:45:36 +0100

    useEfffect.

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const GifGrid = ({category}) => {
+
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+        getGifs();
+    },[]);
 
     const getGifs =async () => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morti&limit=10&api_key=Fy5o4SLF3Q2ij74a1bG1N3NoL20R8Cv8';
@@ -18,11 +23,14 @@ export const GifGrid = ({category}) => {
         console.log(gifs);
     }
 
-    getGifs();
+    // getGifs();
 
     return (
         <div>
             <h3>{category}</h3>
+            <h3>{count}</h3>
+
+            <button onClick={ () => setCount(count + 1)}></button>
         </div>
     )
 }
