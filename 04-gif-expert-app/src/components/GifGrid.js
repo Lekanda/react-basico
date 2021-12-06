b9b2cdce7e1ac114fb6c2022f30d6d5d9ba54cf5
lekanda <abernaolao@gmail.com>
2021-12-06 12:56:43 +0100

    Helpers - getGifs.

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,35 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import { GifGridItem } from './GifGridItem';
+import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({category}) => {
 
     const [images, setimages] = useState([]);
     useEffect(() => {
-        getGifs();
-    },[]);
-
-    const getGifs =async () => {
-        const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morti&limit=10&api_key=Fy5o4SLF3Q2ij74a1bG1N3NoL20R8Cv8';
-
-        const resp = await fetch(url);
-        const {data} = await resp.json();
+        getGifs(category)
+            .then(imgs => setimages(imgs));
         
-        const gifs = data.map(img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
-        // console.log(gifs);
-        setimages(gifs);
-    }
+    },[category]);
 
-    // getGifs();
+    
 
     return (
         <>
-            <h3 >{category}</h3>
+            <h3 className="titulo-cat">{category}</h3>
             <div className="card-grid contenedor">
                 <ol>
                     
