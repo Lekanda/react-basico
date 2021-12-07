4cd822272887a186bd7484b3eb279fd0b5b5b616
lekanda <abernaolao@gmail.com>
2021-12-07 17:34:20 +0100

    Implementar PropTypes y Actualizar SnapShots.

--- 03-counter-app/src/setupTests.js
+++ 03-counter-app/src/setupTests.js
@@ -4,7 +4,6 @@
 
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
 import {createSerializer} from 'enzyme-to-json';
 
 Enzyme.configure({ adapter: new Adapter() });
--- 04-gif-expert-app/src/tests/components/GifGridItem.test.js
+++ 04-gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -4,11 +4,15 @@ import {shallow} from 'enzyme';
 import { GifGridItem } from '../../components/GifGridItem';
 
 
+
 describe('Pruebas en GifGridItem', () => {
+    const title = 'Titulo';
+    const url = 'https://localhost/img.jpg';
     
     test('Debe mostrar el <GifGridItem/>', () => {
-        const wrapper = shallow(<GifGridItem />);
+        const wrapper = shallow(<GifGridItem title={title} url={url}/>);
         
         expect(wrapper).toMatchSnapshot();
     });
 })
+
