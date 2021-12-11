c392f38ac1355d612ac3dbb2dd51dbc86cbf9fdc
lekanda <abernaolao@gmail.com>
2021-12-11 12:27:57 +0100

    useState en profundidad.

---
+++ 05-hokk-app/src/components/01-useState/CounterApp.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import './counter.css'
+
+export const CounterApp = () => {
+
+    const [state, setState] = useState({
+        counter1: 10,
+        counter2: 20,
+        counter3: 30,
+        counter4: 40
+    });
+
+    const {counter1,counter2,counter3,counter4} = state;
+
+
+    return (
+        <>
+            <h1>Counter1 {counter1}</h1>
+            <h1>Counter2 {counter2}</h1>
+            <h1>Counter3 {counter3}</h1>
+            <h1>Counter4 {counter4}</h1>
+            <hr></hr>
+
+            <button 
+                className='btn btn-primary' 
+                onClick={() => {
+                    setState({
+                        ...state,
+                        counter1: counter1 + 1
+                    })
+                }}
+            >
+                +1 Increment
+            </button>
+        </>
+    )
+}
+
+// ...state: copia todo el objeto state y sobreescribe solo lo que se cambia. En esta caso solo el counter1
