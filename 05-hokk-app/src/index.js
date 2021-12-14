a635cec9e02366bea2769eac47d9012ceec0bde0
lekanda <abernaolao@gmail.com>
2021-12-14 10:10:14 +0100

    useLayoutEffect.

--- 05-hokk-app/src/index.js
+++ 05-hokk-app/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Layout } from './components/05-useLayoutEffect/Layout';
 // import { FocusScreen } from './components/04-useRef/FocusScreen';
-import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import { FormWithCustomHooks } from './components/02-useEffect/FormWithCustomHook';
 // import { CounterApp } from './components/01-useState/CounterApp';
@@ -11,7 +12,7 @@ import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 
 
 ReactDOM.render(
-  <RealExampleRef />,
+  <Layout />,
   document.getElementById('root')
 );
 
