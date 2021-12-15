2fc65362310d2498eebf8611a81ba8f451cf571d
lekanda <abernaolao@gmail.com>
2021-12-15 14:42:55 +0100

    Tarea Memorize.

--- 05-hokk-app/src/index.js
+++ 05-hokk-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CallbackHook } from './components/06-memos/CallbackHook';
+// import { CallbackHook } from './components/06-memos/CallbackHook';
+import { Padre } from './components/07-tarea-memo/Padre';
 // import { MemoHook } from './components/06-memos/MemoHook';
 // import { Layout } from './components/05-useLayoutEffect/Layout';
 // import { Memorize } from './components/06-memos/Memorize';
@@ -15,7 +16,7 @@ import { CallbackHook } from './components/06-memos/CallbackHook';
 
 
 ReactDOM.render(
-  <CallbackHook />,
+  <Padre />,
   document.getElementById('root')
 );
 
